488106245d25906dc8fbd91ddaebc59138eeafbd
lehongdanh99 <43432402+lehongdanh99@users.noreply.github.com>
2019-10-04 11:08:09 +0700

    script

--- GymManager.sql
+++ GymManager.sql
@@ -1,6 +1,11 @@
 ﻿use master;
-drop database if exists GYMManager;
 go
+ 
+ --drop database if already exists
+ if DB_ID('GYMManager') is not null
+ drop database GYMManager;
+ go
+
 create database GYMManager;
 go 
 use GYMManager;
@@ -8,11 +13,11 @@ go
 
 --Tài khoản
 create table ACCOUNT(
-username varchar(15) not null,
+username varchar(10) not null,
 passw varchar(15) not null,
 userID int not null,
 --Primarykey--
-constraint pk_username primary key (username)
+constraint pk_username primary key (username),
 );
 go
 
@@ -23,7 +28,7 @@ groupName nvarchar(30) not null,
 leaderFName nvarchar(30) null,
 leaderLName nvarchar(30) null,
 --Primarykey--
-constraint pk_groupID primary key (groupID)
+constraint pk_groupID primary key (groupID),
 );
 go
 
@@ -36,12 +41,13 @@ employeeLName nvarchar(30) null,
 employeeBDate datetime not null,
 employeeAddress nvarchar(30) not null,
 gender bit not null,
-phone int not null,
+phone varchar(10) not null,
 salary int not null,
 employeeIDCard int not null,
 --Primarykey and FK--
 constraint pk_employee primary key (employeeID),
-foreign key(groupID) references GROUPEMPLOYEE on delete set null
+foreign key(groupID) references GROUPEMPLOYEE on delete set null,
+foreign key(phone) references ACCOUNT
 );
 go
 
@@ -77,20 +83,21 @@ memFName nvarchar(20)  null,
 memBDate Datetime not null,
 memAddress nvarchar(30) not null,
 memGender bit not null,
-memPhone int not null,
+memPhone varchar(10) not null,
 cardID int not null,
 note nvarchar(30) null,
 --FK & PK --
 constraint pk_MEMBERS primary key (memID),
-constraint ck_cardID unique (cardID)
+constraint ck_cardID unique (cardID),
+foreign key (memPhone) references ACCOUNT
 );
 go
 
 --Khả năng
 create table ABILITIES
 (
-employeeID varchar(20) not null,
-workID varchar(20) not null,
+employeeID varchar(20) null,
+workID varchar(20) null,
 --FK & PK--
 foreign key (employeeID) references EMPLOYEE on delete set null,
 foreign key (workID) references WORK on delete set null
@@ -102,9 +109,20 @@ create table CLASSROOM
 (
 classID nvarchar(20) not null,
 roomNum nvarchar(20) not null,
-teacher varchar(20) not null, -- ID Work
+teacher varchar(20) null, -- ID Work
 constraint pk_classID primary key (classID),
 foreign key (teacher) references EMPLOYEE on delete set null
+);
+go
+
+--Gói dịch vụ
+create table SERVICEPACK
+(
+serviceID varchar(20) not null,
+serviceNAME nvarchar(20) not null,
+cost int null,
+note nvarchar(20) null,
+constraint pk_serviceID primary key (serviceID),
 );
 go
 
@@ -112,8 +130,8 @@ go
 create table CONTRACTS
 (
 contractID varchar(20) not null,
-cusID varchar(20) not null,
-servicePACK varchar(20) not null,
+cusID varchar(20)  null,
+servicePACK varchar(20)  null,
 dateSigned Datetime not null,
 dateLiquidation Datetime not null,
 conStatus nvarchar(20) null,
@@ -122,14 +140,7 @@ constraint pk_contractID primary key (contractID),
 foreign key (cusID) references MEMBERS on delete set null,
 foreign key (servicePACK) references SERVICEPACK on delete set null,
 );
-go
-create table SERVICEPACK
-(
-serviceID varchar(20) not null,
-serviceNAME nvarchar(20) not null,
-cost int null,
-note nvarchar(20) null,
-);
+
 go
 --Chi tiết hợp đồng
 create table DETAILSCONTRACT 
@@ -138,9 +149,8 @@ contractID varchar(20) not null,
 employeeID varchar(20) null,
 workID varchar(20) null,
 value int not null,
-
 --PK & FK --
-constraint pk_detailscontract primary key (contractID),
+foreign key (contractID) references CONTRACTS,
 foreign key (employeeID) references EMPLOYEE on delete set null,
 foreign key (workID) references WORK on delete set null
 );
@@ -160,14 +170,14 @@ go
 create table RECEIPT
 (
 receiptID varchar(20) not null,
+memID varchar(20) not null,
 repceiptDate Datetime not null,
 repceiptTime Time not null,
 employeeID varchar(20) not null,
 value int not null,
-
 --PK FK--
 constraint pk_receipt primary key (receiptID),
-
+foreign key (memID) references MEMBERS,
 );
 go
 
